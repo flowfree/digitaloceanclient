17aeb0a3bb53edc625e2df79d9e1db8bb671a7c5
Nash <nashruddin.amin@gmail.com>
2020-09-17 21:39:14 +0700

    Added examples

---
+++ examples/list_all_droplets.py
@@ -0,0 +1,13 @@
+import os
+from digitaloceanclient import DigitalOceanClient
+
+
+access_token = os.getenv('ACCESS_TOKEN')
+do = DigitalOceanClient(access_token)
+for droplet in do.droplets.all():
+    print(f'ID = {droplet.id}')
+    print(f'Name = {droplet.name}')
+    print(f'Memory = {droplet.memory} MB')
+    print(f'Image = {droplet.image.name}')
+    print(f'Region = {droplet.region.name}')
+    print(f'Created at = {droplet.created_at}')
