91e9285127d8a68cdbc70c4c76708feb263512a3
Nash <nashruddin.amin@gmail.com>
2020-09-22 16:04:22 +0700

    Refactor unit test

--- tests/test_ssh_keys.py
+++ tests/test_ssh_keys.py
@@ -3,22 +3,23 @@ import responses
 
 @responses.activate
 def test_get_all_keys(client, load_json):
+    json_response = load_json('ssh_key_list.json')
     responses.add(
         responses.GET,
         'https://api.digitalocean.com/v2/account/keys',
-        json=load_json('ssh_key_list.json')
+        json=json_response
     )
 
     keys = client.ssh_keys.all()
 
     key = next(keys)
-    assert key.id == 512189
-    assert key.fingerprint == '3b:16:bf:e4:8b:00:8b:b8:59:8c:a9:d3:f0:19:45:fa'
-    assert key.public_key == 'ssh-rsa AEXAMPLEaC1yc2EAAAADAQABAAAAQQDDHr/jh2Jy4yALcK4JyWbVkPRaWmhck3IgCoeOO3z1e2dBowLh64QAM+Qb72pxekALga2oi4GvT+TlWNhzPH4V example 1'
-    assert key.name == 'My SSH Public Key #1'
+    assert key.id == json_response['ssh_keys'][0]['id']
+    assert key.fingerprint == json_response['ssh_keys'][0]['fingerprint']
+    assert key.public_key == json_response['ssh_keys'][0]['public_key']
+    assert key.name == json_response['ssh_keys'][0]['name']
 
     key = next(keys)
-    assert key.id == 512190
-    assert key.fingerprint == '3b:16:bf:e4:8b:00:8b:b8:59:8c:a9:d3:f0:19:45:fb'
-    assert key.public_key == 'ssh-rsa AEXAMPLEaC1yc2EAAAADAQABAAAAQQDDHr/jh2Jy4yALcK4JyWbVkPRaWmhck3IgCoeOO3z1e2dBowLh64QAM+Qb72pxekALga2oi4GvT+TlWNhzPH4V example 2'
-    assert key.name == 'My SSH Public Key #2'
+    assert key.id == json_response['ssh_keys'][1]['id']
+    assert key.fingerprint == json_response['ssh_keys'][1]['fingerprint']
+    assert key.public_key == json_response['ssh_keys'][1]['public_key']
+    assert key.name == json_response['ssh_keys'][1]['name']
