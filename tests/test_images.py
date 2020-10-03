2e4f41dbb24256303bc6084edf468e23d6680a57
Nash <nashruddin.amin@gmail.com>
2020-10-03 15:35:10 +0700

    Lint with flake8

--- tests/test_images.py
+++ tests/test_images.py
@@ -1,6 +1,6 @@
 import json
 
-import pytest 
+import pytest
 
 import responses
 
@@ -20,46 +20,46 @@ def test_list_all_images(client, load_json):
 
     image_slugs = [i.slug for i in client.images.all()]
     expected_slugs = [
-        'centos-6-x32', 
-        'centos-6-x64', 
-        'ubuntu-16-04-x32', 
-        'freebsd-12-x64', 
-        'rancheros', 
-        'ubuntu-18-04-x64', 
-        'fedora-32-x64', 
-        'centos-8-x64', 
-        'debian-10-x64', 
-        'debian-9-x64', 
-        'ubuntu-16-04-x64', 
-        'freebsd-11-x64-zfs', 
-        'fedora-31-x64', 
-        'ubuntu-20-04-x64', 
-        'freebsd-11-x64-ufs', 
-        'centos-7-x64', 
-        'freebsd-12-x64-zfs', 
-        'skaffolder-18-04', 
-        'izenda-18-04', 
-        'quickcorp-qcobjects-18-04', 
-        'fathom-18-04', 
-        'optimajet-workflowserver-18-04', 
-        'nimbella-18-04', 
-        'snapt-snaptaria-18-04', 
-        'snapt-snaptnova-18-04', 
-        'weconexpbx-7-6', 
-        'bitwarden-18-04', 
-        'buddy-18-04', 
-        'sharklabs-minecraftjavaedi-18-04', 
-        'selenoid-18-04', 
-        'litespeedtechnol-openlitespeednod-18-04', 
-        'simontelephonics-freepbx-7-6', 
-        'gluu-gluuserverce-18-04-3', 
-        'netfoundry-7-6', 
-        'kandralabs-zulip-18-04', 
-        'buddy-repman-18-04', 
-        'strapi-18-04', 
-        'wftutorials-purdm-18-04', 
-        'flipstarter-18-04', 
-        'caprover-18-04', 
+        'centos-6-x32',
+        'centos-6-x64',
+        'ubuntu-16-04-x32',
+        'freebsd-12-x64',
+        'rancheros',
+        'ubuntu-18-04-x64',
+        'fedora-32-x64',
+        'centos-8-x64',
+        'debian-10-x64',
+        'debian-9-x64',
+        'ubuntu-16-04-x64',
+        'freebsd-11-x64-zfs',
+        'fedora-31-x64',
+        'ubuntu-20-04-x64',
+        'freebsd-11-x64-ufs',
+        'centos-7-x64',
+        'freebsd-12-x64-zfs',
+        'skaffolder-18-04',
+        'izenda-18-04',
+        'quickcorp-qcobjects-18-04',
+        'fathom-18-04',
+        'optimajet-workflowserver-18-04',
+        'nimbella-18-04',
+        'snapt-snaptaria-18-04',
+        'snapt-snaptnova-18-04',
+        'weconexpbx-7-6',
+        'bitwarden-18-04',
+        'buddy-18-04',
+        'sharklabs-minecraftjavaedi-18-04',
+        'selenoid-18-04',
+        'litespeedtechnol-openlitespeednod-18-04',
+        'simontelephonics-freepbx-7-6',
+        'gluu-gluuserverce-18-04-3',
+        'netfoundry-7-6',
+        'kandralabs-zulip-18-04',
+        'buddy-repman-18-04',
+        'strapi-18-04',
+        'wftutorials-purdm-18-04',
+        'flipstarter-18-04',
+        'caprover-18-04',
     ]
 
     assert image_slugs == expected_slugs
@@ -75,20 +75,24 @@ def test_create_an_image(client, load_json):
         status=202,
     )
 
-    image = client.images.create(name='ubuntu-18.04-minimal',
-                                 url='http://cloud-images.ubuntu.com/minimal/releases/bionic/release/ubuntu-18.04-minimal-cloudimg-amd64.img',
-                                 region='nyc3',
-                                 distribution='Ubuntu',
-                                 description='Cloud-optimized image w/ small footprint',
-                                 tags=['base-image', 'prod'])
+    image = client.images.create(
+        name='ubuntu-18.04-minimal',
+        url='http://cloud-images.ubuntu.com/minimal/releases/'
+            'bionic/release/ubuntu-18.04-minimal-cloudimg-amd64.img',
+        region='nyc3',
+        distribution='Ubuntu',
+        description='Cloud-optimized image w/ small footprint',
+        tags=['base-image', 'prod']
+    )
 
     assert responses.calls[0].request.body.decode('utf-8') == json.dumps({
-        "name": "ubuntu-18.04-minimal",
-        "url": "http://cloud-images.ubuntu.com/minimal/releases/bionic/release/ubuntu-18.04-minimal-cloudimg-amd64.img",
-        "region": "nyc3",
-        "distribution": "Ubuntu",
-        "description": "Cloud-optimized image w/ small footprint",
-        "tags": ["base-image", "prod"]
+        'name': 'ubuntu-18.04-minimal',
+        'url': 'http://cloud-images.ubuntu.com/minimal/releases/'
+               'bionic/release/ubuntu-18.04-minimal-cloudimg-amd64.img',
+        'region': 'nyc3',
+        'distribution': 'Ubuntu',
+        'description': 'Cloud-optimized image w/ small footprint',
+        'tags': ['base-image', 'prod']
     })
     expected = json_response['image']
     assert image.id == expected['id']
@@ -142,7 +146,8 @@ def test_retrieve_an_image_by_slug(client, load_json):
 
     assert len(responses.calls) == 1
     assert responses.calls[0].request.method == 'GET'
-    assert responses.calls[0].request.url == 'https://api.digitalocean.com/v2/images/sample-snapshot'
+    assert responses.calls[0].request.url == \
+           'https://api.digitalocean.com/v2/images/sample-snapshot'
 
     expected = json_response['image']
     assert image.id == expected['id']
@@ -159,7 +164,7 @@ def test_retrieve_an_image_by_invalid_slug(client):
     )
 
     with pytest.raises(client.NotFound) as e:
-        image = client.images.get(slug='xxxyyyzzz')
+        _ = client.images.get(slug='xxxyyyzzz')
     assert str(e.value) == 'NotFound: The requested image does not exist.'
 
 
@@ -175,7 +180,8 @@ def test_update_an_image(client, load_json):
 
     image = client.images.update('7555620', name='new-image-name')
 
-    assert responses.calls[0].request.body.decode('utf-8') == json.dumps({'name': 'new-image-name'})
+    assert responses.calls[0].request.body.decode('utf-8') == \
+           json.dumps({'name': 'new-image-name'})
 
     expected = json_response['image']
     assert image.id == expected['id']
@@ -205,5 +211,6 @@ def test_delete_an_image(client):
 
     assert len(responses.calls) == 1
     assert responses.calls[0].request.method == 'DELETE'
-    assert responses.calls[0].request.url == 'https://api.digitalocean.com/v2/images/7938391'
-    assert response == None
+    assert responses.calls[0].request.url == \
+           'https://api.digitalocean.com/v2/images/7938391'
+    assert response is None
