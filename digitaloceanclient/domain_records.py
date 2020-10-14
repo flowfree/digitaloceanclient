1cb0c6eb085a1f96fee4f4d57ff5f84f1ce44c52
Nash <nashruddin.amin@gmail.com>
2020-10-14 16:50:52 +0700

    Retrieve all domain records

---
+++ digitaloceanclient/domain_records.py
@@ -0,0 +1,177 @@
+from .http_client import HttpClient
+from .models import DomainRecord
+
+
+class DomainRecords(HttpClient):
+    """
+    Retrieve, create, update, and delete domain records.
+    """
+
+    model = DomainRecord
+
+    def all(self, for_domain, name=None, type_=None):
+        """
+        List all domain records.
+
+        Parameters
+        ----------
+        for_domain : str
+            Get the records for the specified domain.
+        name : str, optional
+            The host name, alias, or service being defined by the record.
+        type_ : str, optional
+            The type of the DNS record.
+
+        Yields
+        ------
+        digitaloceanclient.models.DomainRecord
+
+        Raises
+        ------
+        digitaloceanclient.exceptions.APIError
+        """
+
+        next_url = f'domains/{for_domain}/records'
+        params = {}
+        if name:
+            params['name'] = name
+        if type_:
+            params['type'] = type_
+        while True:
+            response = self._request('GET', next_url, params)
+            for row in response.get('domain_records', []):
+                yield self.model(row)
+            try:
+                next_url = response['links']['pages']['next']
+            except KeyError:
+                break
+
+    def create(self, for_domain, type_, name=None, data=None, priority=None,
+               port=None, ttl=None, weight=None, flags=None, tag=None):
+        """
+        Create a new record for a domain.
+
+        Parameters
+        ----------
+        for_domain : str
+            The domain for the record to be added to.
+        type_ : str
+            The record type.
+        name : str, optional
+            The host name, alias, or service being defined by the record.
+            Required for: A, AAAA, CAA, CNAME, TXT, SRV
+        data: str, optional
+            The data for the domain record.
+            Required for: A, AAAA, CAA, CNAME, MX, TXT, SRV, NS
+        priority : int, optional
+            The priority of the host.
+            Required for: MX, SRV
+        port : int, optional
+            The port that the service is accessible on.
+            Required for: SRV
+        ttl: int, optional
+            This value is the time to live for the record, in seconds.
+            Required for: SOA
+        weight: int, optional
+            The weight of records with the same priority.
+            Required for: SRV
+        flags: int, optional
+            An unsigned integer between 0-255 used for CAA records.
+        tag: {"issue", "issuewild", "iodef"}, optional
+            The parameter tag for CAA records.
+
+        Returns
+        -------
+        digitaloceanclient.models.DomainRecord
+
+        Raises
+        ------
+        digitaloceanclient.exceptions.APIError
+        """
+
+        raise NotImplementedError
+
+    def get(self, for_domain, record_id):
+        """
+        Retrieve an existing domain record.
+
+        Parameters
+        ----------
+        for_domain : str
+            The domain name where this record exist.
+        record_id : str
+            The identifier for the domain record.
+
+        Returns
+        -------
+        digitaloceanclient.models.DomainRecord
+
+        Raises
+        ------
+        digitaloceanclient.exceptions.APIError
+        """
+
+        raise NotImplementedError
+
+    def update(self, for_domain, type_, name=None, data=None, priority=None,
+               port=None, ttl=None, weight=None, flags=None, tag=None):
+        """
+        Update existing domain record.
+
+        Parameters
+        ----------
+        for_domain : str
+            The domain where the record exist.
+        type_ : str
+            The record type.
+        name : str, optional
+            The host name, alias, or service being defined by the record.
+            Available for: A, AAAA, CAA, CNAME, TXT, SRV
+        data: str, optional
+            The data for the domain record.
+            Available for: A, AAAA, CAA, CNAME, MX, TXT, SRV, NS
+        priority : int, optional
+            The priority of the host.
+            Available for: MX, SRV
+        port : int, optional
+            The port that the service is accessible on.
+            Available for: SRV
+        ttl: int, optional
+            This value is the time to live for the record, in seconds.
+            Available for: SOA
+        weight: int, optional
+            The weight of records with the same priority.
+            Available for: SRV
+        flags: int, optional
+            An unsigned integer between 0-255 used for CAA records.
+        tag: {"issue", "issuewild", "iodef"}, optional
+            The parameter tag for CAA records.
+
+        Returns
+        -------
+        digitaloceanclient.models.DomainRecord
+
+        Raises
+        ------
+        digitaloceanclient.exceptions.APIError
+        """
+
+        raise NotImplementedError
+
+    def delete(self, for_domain, record_id):
+        """
+        Delete a domain record.
+
+        Parameters
+        ----------
+        for_domain : str
+            The domain name where this record exist.
+        record_id : str
+            The identifier for the domain record.
+
+        Raises
+        ------
+        digitaloceanclient.exceptions.APIError
+        """
+
+        raise NotImplementedError
---
+++ tests/test_domain_records.py
@@ -0,0 +1,65 @@
+import pytest
+import responses
+
+from .models.test_domain_record import domain_record_model_matches
+
+
+@responses.activate
+def test_retrieve_all_records(client, load_json):
+    json_response = load_json('domain_record_list.json')
+    responses.add(
+        responses.GET,
+        'https://api.digitalocean.com/v2/domains/example.com/records',
+        json=json_response,
+        status=200,
+    )
+
+    records = client.domain_records.all(for_domain='example.com')
+
+    for expected in json_response['domain_records']:
+        record = next(records)
+        assert domain_record_model_matches(record, expected)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.method == 'GET'
+    assert responses.calls[0].request.url == \
+           'https://api.digitalocean.com/v2/domains/example.com/records'
+
+
+@responses.activate
+def test_filter_domain_records(client, load_json):
+    json_response = load_json('domain_record_list.json')
+    responses.add(
+        responses.GET,
+        'https://api.digitalocean.com/v2/domains/example.com/records',
+        json=json_response,
+        status=200,
+    )
+
+    records = client.domain_records.all(for_domain='example.com', 
+                                        name='sub.example.com')
+    next(records)
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.method == 'GET'
+    assert responses.calls[0].request.url == \
+           'https://api.digitalocean.com/v2' \
+           '/domains/example.com/records?name=sub.example.com'
+
+    records = client.domain_records.all(for_domain='example.com', 
+                                        type_='MX')
+    next(records)
+    assert len(responses.calls) == 2
+    assert responses.calls[1].request.method == 'GET'
+    assert responses.calls[1].request.url == \
+           'https://api.digitalocean.com/v2' \
+           '/domains/example.com/records?type=MX'
+
+    records = client.domain_records.all(for_domain='example.com', 
+                                        name='sub.example.com',
+                                        type_='MX')
+    next(records)
+    assert len(responses.calls) == 3
+    assert responses.calls[2].request.method == 'GET'
+    assert responses.calls[2].request.url == \
+           'https://api.digitalocean.com/v2' \
+           '/domains/example.com/records?name=sub.example.com&type=MX'
